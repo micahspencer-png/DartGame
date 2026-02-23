2fe41577e081f67b51ec4d175e17fa5e70d04835
micahspencer-png <micahspencer@isu.edu>
2026-02-22 19:53:05 -0700

    added dartboard image to screen. added function to draw darts onto board. still need to debug the screen

--- DartGame/DartGame.Designer.cs
+++ DartGame/DartGame.Designer.cs
@@ -30,32 +30,93 @@
         {
             this.DisplayPictureBox = new System.Windows.Forms.PictureBox();
             this.ExitButton = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.PlayRadioButton = new System.Windows.Forms.RadioButton();
+            this.ReviewRadioButton = new System.Windows.Forms.RadioButton();
+            this.Displaylabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.DisplayPictureBox)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // DisplayPictureBox
             // 
-            this.DisplayPictureBox.Location = new System.Drawing.Point(96, 12);
+            this.DisplayPictureBox.Location = new System.Drawing.Point(12, 12);
             this.DisplayPictureBox.Name = "DisplayPictureBox";
-            this.DisplayPictureBox.Size = new System.Drawing.Size(586, 457);
+            this.DisplayPictureBox.Size = new System.Drawing.Size(550, 550);
             this.DisplayPictureBox.TabIndex = 0;
             this.DisplayPictureBox.TabStop = false;
             // 
             // ExitButton
             // 
-            this.ExitButton.Location = new System.Drawing.Point(688, 326);
+            this.ExitButton.Location = new System.Drawing.Point(668, 426);
             this.ExitButton.Name = "ExitButton";
-            this.ExitButton.Size = new System.Drawing.Size(115, 71);
+            this.ExitButton.Size = new System.Drawing.Size(143, 71);
             this.ExitButton.TabIndex = 1;
             this.ExitButton.Text = "E&xit";
             this.ExitButton.UseVisualStyleBackColor = true;
             this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(629, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(174, 24);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.PlayRadioButton);
+            this.groupBox1.Controls.Add(this.ReviewRadioButton);
+            this.groupBox1.Location = new System.Drawing.Point(629, 193);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(154, 145);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // PlayRadioButton
+            // 
+            this.PlayRadioButton.AutoSize = true;
+            this.PlayRadioButton.Location = new System.Drawing.Point(26, 87);
+            this.PlayRadioButton.Name = "PlayRadioButton";
+            this.PlayRadioButton.Size = new System.Drawing.Size(55, 20);
+            this.PlayRadioButton.TabIndex = 1;
+            this.PlayRadioButton.TabStop = true;
+            this.PlayRadioButton.Text = "Play";
+            this.PlayRadioButton.UseVisualStyleBackColor = true;
+            this.PlayRadioButton.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.DartGame_KeyPress);
+            // 
+            // ReviewRadioButton
+            // 
+            this.ReviewRadioButton.AutoSize = true;
+            this.ReviewRadioButton.Location = new System.Drawing.Point(26, 50);
+            this.ReviewRadioButton.Name = "ReviewRadioButton";
+            this.ReviewRadioButton.Size = new System.Drawing.Size(73, 20);
+            this.ReviewRadioButton.TabIndex = 0;
+            this.ReviewRadioButton.TabStop = true;
+            this.ReviewRadioButton.Text = "Review";
+            this.ReviewRadioButton.UseVisualStyleBackColor = true;
+            this.ReviewRadioButton.CheckedChanged += new System.EventHandler(this.ReviewRadioButton_CheckedChanged);
+            // 
+            // Displaylabel
+            // 
+            this.Displaylabel.AutoSize = true;
+            this.Displaylabel.Location = new System.Drawing.Point(604, 97);
+            this.Displaylabel.Name = "Displaylabel";
+            this.Displaylabel.Size = new System.Drawing.Size(207, 16);
+            this.Displaylabel.TabIndex = 4;
+            this.Displaylabel.Text = "Press SpaceBar To Launch Darts";
+            // 
             // DartGame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(823, 509);
+            this.ClientSize = new System.Drawing.Size(823, 574);
+            this.Controls.Add(this.Displaylabel);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.ExitButton);
             this.Controls.Add(this.DisplayPictureBox);
             this.MaximizeBox = false;
@@ -63,8 +124,12 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "DartGame";
             this.Load += new System.EventHandler(this.DartGame_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.DartGame_KeyPress);
             ((System.ComponentModel.ISupportInitialize)(this.DisplayPictureBox)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -72,6 +137,11 @@
 
         private System.Windows.Forms.PictureBox DisplayPictureBox;
         private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton PlayRadioButton;
+        private System.Windows.Forms.RadioButton ReviewRadioButton;
+        private System.Windows.Forms.Label Displaylabel;
     }
 }
 
--- DartGame/DartGame.cs
+++ DartGame/DartGame.cs
@@ -3,42 +3,116 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace DartGame
 {
+    //Micah Spencer
+    //RCET 3371
+    //Spring 2026
+    //Dart Game Program
+    //
     public partial class DartGame : Form
     {
+        int DartCount = 0;
+        string DataFile = "..\\..\\..\\Dart.Txt";
+        string DartData = "";
+        string[,] DartArray;
+        Random DartX = new Random();
+        Random DartY = new Random();
+
         public DartGame()
         {
+            
             InitializeComponent();
+            SetDefaults();
+            DrawBoard();
         }
         //Program Logic--------------------------------------------------------------------------------------------------------------------------
 
+        void SetDefaults() 
+        { 
+            ReviewRadioButton.Checked = true;
+            PlayRadioButton.Checked = false;
+            DrawBoard();
+        }
         void DrawBoard() 
         {
             Graphics g = DisplayPictureBox.CreateGraphics();
             Pen thePen = new Pen(Color.Black);
-            System.Drawing.Image image = System.Drawing.Image.FromFile("C:\\Users\\Micas\\OneDrive\\Documents\\Git\\Images\\DartBoard.jpg");
+            System.Drawing.Image image = System.Drawing.Image.FromFile("..\\..\\..\\DartBoard.jpg");
 
-            g.DrawImage(image, 0, 0);
+            g.DrawImage(image, 0, 0); 
 
             g.Dispose();
             thePen.Dispose();
+        }
+        void DrawDart(int x, int y)
+        {
+            Graphics g = DisplayPictureBox.CreateGraphics();
+            Pen thePen = new Pen(Color.Blue, 1);
+            int size = 25;
+
+            g.DrawEllipse(thePen, x - size / 2, y - size / 2, size, size);
+            g.DrawLine(thePen, x - 3, y, x + 3, y);
+            g.DrawLine(thePen, x, y - 3, x, y + 3);
+            g.Dispose();
+            thePen.Dispose();
+        }
+
+        void Play() 
+        {
+            DartCount = DartCount + 1;
+            if (DartCount == 4)
+            {
+                Displaylabel.Text = "Press Space to Start again";
+                DartCount = 0;
+            }
+            else
+            {
+                int x = DartX.Next(1,579);
+                int y = DartY.Next(1,579);
+
+                DrawDart(x,y);
+            }
+        }
+
+        void Review() 
+        { 
+        
         }
 
         //Event Handlers----------------------------------------------------------------------------------------------------------------------------
         private void DartGame_Load(object sender, EventArgs e)
         {
-            DrawBoard();
+            
         }
 
         private void ExitButton_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void ReviewRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (ReviewRadioButton.Checked == true) 
+            {
+                SetDefaults();
+                Review();
+            }
+        }
+
+        private void DartGame_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == ' ') 
+            {
+                Play();
+            }
         }
     }
 }
