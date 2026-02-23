25e38a777bebd5809e3ef038099ad73427bab7ee
micahspencer-png <micahspencer@isu.edu>
2026-02-22 18:48:00 -0700

    Add project files.

---
+++ DartGame/DartGame.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace DartGame
+{
+    partial class DartGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DisplayPictureBox = new System.Windows.Forms.PictureBox();
+            this.ExitButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DisplayPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DisplayPictureBox
+            // 
+            this.DisplayPictureBox.Location = new System.Drawing.Point(96, 12);
+            this.DisplayPictureBox.Name = "DisplayPictureBox";
+            this.DisplayPictureBox.Size = new System.Drawing.Size(586, 457);
+            this.DisplayPictureBox.TabIndex = 0;
+            this.DisplayPictureBox.TabStop = false;
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(688, 326);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(115, 71);
+            this.ExitButton.TabIndex = 1;
+            this.ExitButton.Text = "E&xit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // DartGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(823, 509);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.DisplayPictureBox);
+            this.MaximizeBox = false;
+            this.Name = "DartGame";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DartGame";
+            this.Load += new System.EventHandler(this.DartGame_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DisplayPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox DisplayPictureBox;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
+
---
+++ DartGame/DartGame.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DartGame
+{
+    public partial class DartGame : Form
+    {
+        public DartGame()
+        {
+            InitializeComponent();
+        }
+        //Program Logic--------------------------------------------------------------------------------------------------------------------------
+
+        void DrawBoard() 
+        {
+            Graphics g = DisplayPictureBox.CreateGraphics();
+            Pen thePen = new Pen(Color.Black);
+            System.Drawing.Image image = System.Drawing.Image.FromFile("C:\\Users\\Micas\\OneDrive\\Documents\\Git\\Images\\DartBoard.jpg");
+
+            g.DrawImage(image, 0, 0);
+
+            g.Dispose();
+            thePen.Dispose();
+        }
+
+        //Event Handlers----------------------------------------------------------------------------------------------------------------------------
+        private void DartGame_Load(object sender, EventArgs e)
+        {
+            DrawBoard();
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
